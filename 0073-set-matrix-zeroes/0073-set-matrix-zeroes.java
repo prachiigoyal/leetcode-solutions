69c4b4d4d2b06464d1afa4c88f621d78ddeb0c4e
prachiigoyal <66202122+prachiigoyal@users.noreply.github.com>
2022-10-19 16:20:04 +0530

    Time: 1 ms (97.43%), Space: 54 MB (55.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,23 +1,48 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rows=new HashSet<>();
-        HashSet<Integer> cols=new HashSet<>();
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        boolean row=false,col=false;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0){
+                col=true;
+            }
+        }
+        for(int j=0;j<m;j++){
+            if(matrix[0][j]==0){
+                row=true;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
-                    rows.add(i);
-                    cols.add(j);
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int row:rows){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[row][j]=0;
+        for(int i=1;i<n;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<m;j++){
+                    matrix[i][j]=0;
+                }
             }
         }
-        for(int col:cols){
-            for(int j=0;j<matrix.length;j++){
-                matrix[j][col]=0;
+        for(int j=1;j<m;j++){
+            if(matrix[0][j]==0){
+                for(int i=0;i<n;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(row){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
     }

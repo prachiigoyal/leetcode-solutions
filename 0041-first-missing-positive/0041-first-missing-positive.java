947f3c9b0ca7b115754d7820b64e561c4c4887ba
prachiigoyal <66202122+prachiigoyal@users.noreply.github.com>
2022-10-20 12:19:19 +0530

    Time: 2 ms (83.36%), Space: 57.3 MB (66.77%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,16 +1,26 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        int max=0;
+        //mark elements as 1 if they are out of ranges and maintain presence of 1
+        boolean one=false;
         for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-            max=Math.max(max,nums[i]);
-        }
-        for(int i=1;i<=max;i++){
-            if(set.contains(i)==false){
-                return i;
+            int ele=nums[i];
+            if(ele==1){
+                one=true;
+            }
+            if(ele<1 || ele>nums.length){
+                nums[i]=1;
             }
         }
-        return max+1;
+        if(one==false) return 1;
+        //map the index and make it negative
+        for(int i=0;i<nums.length;i++){
+            int index=Math.abs(nums[i]);
+            nums[index-1]=-Math.abs(nums[index-1]);
+        }
+        // positive number -> index+1 is missing
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0) return i+1;
+        }
+        return nums.length+1;
     }
 }

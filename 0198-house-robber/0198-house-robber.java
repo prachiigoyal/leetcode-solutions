1f20feaab80c90615ff559085c653b0804a3cca2
prachiigoyal <66202122+prachiigoyal@users.noreply.github.com>
2022-11-08 16:08:30 +0530

    Time: 0 ms (100.00%), Space: 41 MB (77.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,25 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int[][] dp = new int[nums.length][2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j]=-1;
-            }
+        int[][] dp=new int[nums.length][2];
+        dp[0][0]=nums[0];
+        dp[0][1]=0;
+        for(int i=1;i<nums.length;i++){
+            dp[i][0]=nums[i]+dp[i-1][1];
+            dp[i][1]=Math.max(dp[i-1][0],dp[i-1][1]);
         }
-        return helper(nums,0,false,dp);
-    }
-    public int helper(int[] nums, int idx, boolean isLastIncluded,int[][] dp){
-        if(idx==nums.length){
-            return 0;
-        }
-        if(dp[idx][isLastIncluded?0:1]!=-1){
-            return dp[idx][isLastIncluded?0:1];
-        }
-        if(isLastIncluded){
-            return dp[idx][isLastIncluded?0:1] = helper(nums,idx+1,false,dp);
-        }
-        else{
-            return dp[idx][isLastIncluded?0:1] = Math.max(helper(nums,idx+1,false,dp),nums[idx]+helper(nums,idx+1,true,dp));
-        }
+        return Math.max(dp[nums.length-1][0],dp[nums.length-1][1]);
     }
 }

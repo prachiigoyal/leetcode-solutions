fd2b18615d7886b34fa162d9e3f41bd36ece8320
prachiigoyal <66202122+prachiigoyal@users.noreply.github.com>
2022-11-11 12:22:36 +0530

    Time: 3 ms (57.74%), Space: 48.3 MB (54.23%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -2,8 +2,7 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> ans=new ArrayList<>();
         
-        for(int i=0;i<n;i++){
-            int index=i+1;
+        for(int index=1;index<=n;index++){
             if(index%3==0 && index%5==0){
                 ans.add("FizzBuzz");
             }else if(index%5==0){
